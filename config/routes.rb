c886560f8be9a8b05047a0c099c6471e37f7e4fc
Raghu Betina <raghu@firstdraft.com>
2018-12-06 19:30:31 +0000

    Display associated collections on show pages

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_venue/:id_to_remove", { :controller => "venues", :action => "destroy_row" })
+  get("/delete_venue_from_neighborhood/:id_to_remove", { :controller => "venues", :action => "destroy_row_from_neighborhood" })
 
   #------------------------------
 
@@ -74,6 +75,9 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_bookmark/:id_to_remove", { :controller => "bookmarks", :action => "destroy_row" })
+  get("/delete_bookmark_from_user/:id_to_remove", { :controller => "bookmarks", :action => "destroy_row_from_user" })
+  get("/delete_bookmark_from_venue/:id_to_remove", { :controller => "bookmarks", :action => "destroy_row_from_venue" })
+  get("/delete_bookmark_from_dish/:id_to_remove", { :controller => "bookmarks", :action => "destroy_row_from_dish" })
 
   #------------------------------
 
@@ -93,6 +97,7 @@ Rails.application.routes.draw do
 
   # DELETE
   get("/delete_dish/:id_to_remove", { :controller => "dishes", :action => "destroy_row" })
+  get("/delete_dish_from_cuisine/:id_to_remove", { :controller => "dishes", :action => "destroy_row_from_cuisine" })
 
   #------------------------------
 
